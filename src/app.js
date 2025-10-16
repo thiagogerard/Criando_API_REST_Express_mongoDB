f7fbe2168a3b45b37c56d7a069cfe8c06cd66f99
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-10-16 12:07:50 -0700

    feat: add endpoint to retrieve list of books

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
+const livros = [
+    {
+        id: 1,
+        titulo: "O senhor dos aneis"
+    },
+    {
+        id: 2,
+        titulo: "O hobbit"
+    }
+]
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
 });
 
 export default app;
