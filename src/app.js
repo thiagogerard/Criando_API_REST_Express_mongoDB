60d9476f77a65a422c77e4c980e9daf5d048d633
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-10-21 12:12:06 -0700

    feat: implement MongoDB model for books and update GET endpoint to fetch from database

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import conectaNaDataBase from "./config/dbConnect.js";
+import livro from "./models/Livro.js"
 
 const conexao = await conectaNaDataBase();
 
@@ -14,29 +15,13 @@ conexao.once("open", () => {
 const app = express();
 app.use(express.json())
 
-const livros = [
-    {
-        id: 1,
-        titulo: "O senhor dos aneis"
-    },
-    {
-        id: 2,
-        titulo: "O hobbit"
-    }
-]
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    })
-}
-
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {
